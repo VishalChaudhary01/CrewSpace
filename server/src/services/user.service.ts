b4f3c4347c5b8f82c8d22f119f4e13f9d7262915
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-05-30 00:54:42 +0530

    refactor: update profile api

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,6 +1,5 @@
 import { UserModel } from "@/models/user.model";
 import { BadRequestError } from "@/errors/bad-request.error";
-import { WorkspaceModel } from "@/models/wrokspace.model";
 
 export const getCurrentUserService = async (userId: string) => {
   const user = await UserModel.findById(userId)
@@ -10,8 +9,6 @@ export const getCurrentUserService = async (userId: string) => {
   if (!user) {
     throw new BadRequestError("User not Found");
   }
-  const works = await WorkspaceModel.find({ userId: user._id });
-  console.log(works, user);
 
   return { user };
 };
