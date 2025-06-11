21499cedfde6360befe053cb88aa449827a33394
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-06-12 02:37:22 +0530

    fix: add return types to hooks

--- client/src/hooks/index.tsx
+++ client/src/hooks/index.tsx
@@ -9,12 +9,25 @@ import {
   getProjectsInWorkspaceQueryFn,
   getWorkspaceWithMembersQueryFn,
 } from "@/lib/api";
-import type { WorkspaceRequest } from "@/types/workspace.type";
-import type { AllProjectRequest, ProjectRequest } from "@/types/project.type";
-import type { AllTaskRequest } from "@/types/task.type";
+import type {
+  AllWorkspacesResponse,
+  GetMembersInWorkspaceResponse,
+  WorkspaceRequest,
+  WorkspaceWithMembersResponse,
+} from "@/types/workspace.type";
+import type { CustomError } from "@/types/error.type";
+import type { UserResponse } from "@/types/user.type";
+import type { AnalyticsResponse } from "@/types/common.type";
+import type { AllTaskRequest, AllTaskResponse } from "@/types/task.type";
+import type {
+  AllProjectRequest,
+  AllProjectResponse,
+  ProjectRequest,
+  ProjectResponse,
+} from "@/types/project.type";
 
 export const useAuth = () => {
-  const query = useQuery({
+  const query = useQuery<UserResponse, CustomError>({
     queryKey: ["auth-user"],
     queryFn: getCurrentUserQueryFn,
     staleTime: 0,
@@ -25,7 +38,7 @@ export const useAuth = () => {
 };
 
 export const useGetWorkspace = ({ workspaceId }: WorkspaceRequest) => {
-  const query = useQuery({
+  const query = useQuery<WorkspaceWithMembersResponse, CustomError>({
     queryKey: ["workspace", workspaceId],
     queryFn: () => getWorkspaceWithMembersQueryFn({ workspaceId }),
     staleTime: 0,
@@ -37,7 +50,7 @@ export const useGetWorkspace = ({ workspaceId }: WorkspaceRequest) => {
 };
 
 export const useGetMembersInWorkspace = ({ workspaceId }: WorkspaceRequest) => {
-  const query = useQuery({
+  const query = useQuery<GetMembersInWorkspaceResponse, CustomError>({
     queryKey: ["members", workspaceId],
     queryFn: () => getMembersInWorkspaceQueryFn({ workspaceId }),
     staleTime: Infinity,
@@ -47,7 +60,7 @@ export const useGetMembersInWorkspace = ({ workspaceId }: WorkspaceRequest) => {
 };
 
 export const useGetAllWorkspacesUserIsMember = () => {
-  const query = useQuery({
+  const query = useQuery<AllWorkspacesResponse, CustomError>({
     queryKey: ["user-workspaces"],
     queryFn: () => getAllWorkspacesUserIsMemberQueryFn(),
     staleTime: Infinity,
@@ -62,7 +75,7 @@ export const useGetProjectsInWorkspace = ({
   pageNumber,
   skip = false,
 }: AllProjectRequest) => {
-  const query = useQuery({
+  const query = useQuery<AllProjectResponse, CustomError>({
     queryKey: ["all-projects", workspaceId, pageNumber, pageSize],
     queryFn: () =>
       getProjectsInWorkspaceQueryFn({
@@ -78,11 +91,11 @@ export const useGetProjectsInWorkspace = ({
   return query;
 };
 
-export const useGetProjectAnalytics = (
-  workspaceId: string,
-  projectId: string,
-) => {
-  const query = useQuery({
+export const useGetProjectAnalytics = ({
+  workspaceId,
+  projectId,
+}: ProjectRequest) => {
+  const query = useQuery<AnalyticsResponse, CustomError>({
     queryKey: ["project-analytics", projectId],
     queryFn: () => getProjectAnalyticsQueryFn({ workspaceId, projectId }),
     staleTime: 0,
@@ -96,7 +109,7 @@ export const useGetProjectById = ({
   workspaceId,
   projectId,
 }: ProjectRequest) => {
-  const query = useQuery({
+  const query = useQuery<ProjectResponse, CustomError>({
     queryKey: ["project", projectId],
     queryFn: () =>
       getProjectByIdQueryFn({
@@ -117,7 +130,7 @@ export const useGetAllTasks = ({
   pageSize,
   pageNumber,
 }: AllTaskRequest) => {
-  const query = useQuery({
+  const query = useQuery<AllTaskResponse, CustomError>({
     queryKey: ["all-tasks", workspaceId, pageSize, pageNumber, filters],
     queryFn: () =>
       getAllTasksQueryFn({
