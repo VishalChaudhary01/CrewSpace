0f832979ee63c40b49d811ffa37a782f7badb4f1
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-06-14 13:19:51 +0530

    feat: update tasks page

--- client/src/pages/workspace/tasks.page.tsx
+++ client/src/pages/workspace/tasks.page.tsx
@@ -1,3 +1,32 @@
+import { Plus } from "lucide-react";
+import { Permissions } from "@/constants";
+import { useCreateTaskDialog } from "@/hooks/dialog";
+import { Button } from "@/components/ui/button";
+import { PermissionsGuard } from "@/components/common/permission-guard";
+
 export const TasksPage = () => {
-  return <div>TasksPage</div>;
+  const { onOpen: onOpenCreateTask } = useCreateTaskDialog();
+
+  return (
+    <div className='w-full h-full flex-col space-y-8 pt-3'>
+      <div className='flex items-center justify-between space-y-2'>
+        <div>
+          <h2 className='text-2xl font-bold tracking-tight'>All Tasks</h2>
+          <p className='text-muted-foreground'>
+            Here&apos;s the list of tasks for this workspace!
+          </p>
+        </div>
+        <PermissionsGuard requiredPermission={Permissions.CREATE_TASK}>
+          <Button onClick={() => setTimeout(() => onOpenCreateTask(), 100)}>
+            <Plus className='w-4 h-4 ' />
+            <span>Create Task</span>
+          </Button>
+        </PermissionsGuard>
+      </div>
+      {/* TODO - NEED TO ADD TABLE FOR ALL TASKS */}
+      <span>
+        <strong>Todo: </strong>Need to add Tasks table
+      </span>
+    </div>
+  );
 };
